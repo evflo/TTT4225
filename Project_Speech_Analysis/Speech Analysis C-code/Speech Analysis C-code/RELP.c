f5353c11cabddf0ae841b7ccfa6b7e68dff87b30
lghfff <lhgfff@fkfk.com>
2015-03-10 15:09:14 +0100

    Include RELP

---
+++ Project_Speech_Analysis/Speech Analysis C-code/Speech Analysis C-code/RELP.c
@@ -0,0 +1,62 @@
+//
+//  RELP.c
+//  Speech Analysis C-code
+//
+//  Created by Even on 05/03/15.
+//  Copyright (c) 2015 Even. All rights reserved.
+//
+
+#include "RELP.h"
+#include "signalProcessing.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+void RELPcoder(float* data, float* output,int P){
+    
+    const int N = sizeof(data);
+    int filterOrden = 8;
+    int Fs = 16000;
+    int Fc = 2000;
+    int D = 4;
+    int low_P = 4;
+    int step = 0.02*Fs;
+    int halfStep = step/2;
+    float syntheticError[N],syntheticSpeechUpsampled[N],syntheticSpeechHF[N];
+    float windowSpeech[step];
+    
+    
+    //Make filters
+    
+    int i;
+    for (i=0.03*Fs; i<N-0.025*Fs; i+= step) {
+        int lastSpeech = i+1-0.015*Fs;
+        int nextSpeech = i+0.015*Fs;
+        int j;
+        float speechFilt[step];
+        for (j = lastSpeech; j<nextSpeech; j++) {
+            speechFilt[j]= windowSpeech[j]*data[lastSpeech+j];
+        }
+        float ry[nextSpeech-lastSpeech],A[P];
+        autocorr(speechFilt, ry);
+        LevinsonDurbin(ry, A, P);
+        float B[1] = {1};
+        float speechEst[step],speechError[step];
+        filtrate(speechEst, B, 1, A, P, speechFilt);
+        
+        for (j=0; j< step; j++) {
+            speechError[j] = speechFilt[j]-speechEst[j];
+            syntheticError[i+j-halfStep] = speechError[j];
+        }
+        
+        //Filtrate speechError
+        //Decimate speechError
+        //Upsample speechError
+        
+        
+    }
+    
+}

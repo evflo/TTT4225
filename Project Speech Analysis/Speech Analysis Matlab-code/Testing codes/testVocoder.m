d64a101a9b783e8e815c0817f45753131d6940fc
Even Flørenæs <evenflo@stud.ntnu.no>
2015-03-17 12:42:05 +0100

    Commented the code for testing functions

--- Project Speech Analysis/Speech Analysis Matlab-code/Testing codes/testVocoder.m
+++ Project Speech Analysis/Speech Analysis Matlab-code/Testing codes/testVocoder.m
@@ -1,27 +1,37 @@
+%testVocoder is a test function for basicVocoder for different values of P
+%to be used to answer the group specific task.
+%The function plays the output of basicVocoder for randomly picked P
+%to make the review of quality be as precise as possible
 function testVocoder()
 
-    P = [2 4 6 8 10 20]; %Different P
+	%Different P-values to be tested
+    P = [2 4 6 8 10 20]; 
+	
     Fs = 16000; %Sampling frequency
-    pause = zeros(1,10*Fs); %Have pause signal which gives time to subjectively review the signal
-
+    pause = zeros(1,10*Fs); %10 sec pause signal which gives time to subjectively review the signal
+	%First playing the original signal
     fprintf('Play of original\n');
     soundsc(basicVocoder('anvsb1.wav',14),Fs);
     soundsc(pause,Fs)
     while ~isempty(P)
-        k = ceil(rand*length(P)); %Find a random P
-        x = basicVocoder('anvsb1.wav',P(k)); %Find output for a random P
-
-        text = sprintf('Plot for P = %d',P(k));
-
+		%Randomly choosen value of P to be played
+        k = ceil(rand*length(P));
+		
+		%Find output for a random P
+        x = basicVocoder('anvsb1.wav',P(k)); 
+		
+		%Play of output for choosen P followed by a pause
         fprintf('Output for P = %d is now being played \n',P(k)); 
-
-        soundsc(x,Fs); %Play of output for choosen P
+        soundsc(x,Fs); 
         soundsc(pause,Fs);
 
-
+		%Plot of the signal is also presented to be used in review
         figure;
-        plot(x),title(text); %Plot for choosen P
-        P(k) = []; %Discard P-values that have been tested
+		text = sprintf('Plot for P = %d',P(k));
+        plot(x),title(text);
+		
+		%Discard P-values that have been tested
+        P(k) = []; 
 
-    end
-end
+    end %while
+end %function

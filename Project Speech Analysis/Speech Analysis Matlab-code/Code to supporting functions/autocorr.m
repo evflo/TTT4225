209d817330d1d44fae41c3bb7082b0f36444a931
Even Flørenæs <evenflo@stud.ntnu.no>
2015-03-17 12:24:16 +0100

    Commented the code of supporting functions in matlab

--- Project Speech Analysis/Speech Analysis Matlab-code/Code to supporting functions/autocorr.m
+++ Project Speech Analysis/Speech Analysis Matlab-code/Code to supporting functions/autocorr.m
@@ -1,25 +1,21 @@
-%Autokorrelasjonsfunksjon som kan lettere implementeres i C
-%Lager tosidig spekter med r(0) som middelpunkt
-%(Kan endres til ? bli ensidig med r(0)-energien som startpunkt)
-%Tilsvarer n? xcorr-funksjonen
+%Auto correlation function which is easier to implement in C
+%Finds the auto correlation with the signal energy r(0) as starting point
 function r = autocorr(x)
-x = x';
-N = length(x);
-x = [x,zeros(1,N+1)];
-C = zeros(1,N); %Lagrer autokorrelasjonsverdiene i C i iterasjonene
-for k = 1:N
-    for n = 1:N
-        C(k) = C(k)+ x(n)*x(n+k-1);
-        
-    end
-end
-%F?lgende kan sl?yfes og erstattes med r = C; for ensidig spekter med energien som
-%startverdi
-%r = zeros(1,2*length(x)-1); 
-%r((length(C)+1):(length(r))-1) = C;
-%for i = 1:length(C)
-%    r(length(C)-i+1) = C(i);
-%end
-r = C(1:N);
-r = r';
-end
+	x = x';
+	N = length(x);
+	x = [x,zeros(1,N+1)];
+	%Stores the auto correlation values in C in the iterations
+	C = zeros(1,N);
+
+	%Compute the auto correlation using the formula definiton of correlation
+	for k = 1:N
+		for n = 1:N
+			C(k) = C(k)+ x(n)*x(n+k-1);
+		end %for n
+	end %for k
+
+	%Copies the wanted area of C to the auto correlation output
+	r = C(1:N);
+	r = r';
+
+end %function
